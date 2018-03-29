36800e96ebbbd018f98f4b993fdb5b267f80bfd6
precioustosin <precioustosin@hotmail.com>
2018-03-29 10:21:51 +0100

    added functions to call posts page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../public/stylesheets/styles.css';
 
-console.log('SOURCE FILE REACHED');
+import appendPostBody from './ui';
+
+const $ = require('jquery');
+
+function displayPosts() {
+  const element = $('.headlines--container');
+  appendPostBody(element);
+}
+
+$(document).ready(() => {
+  displayPosts();
+});
+
