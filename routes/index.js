0296fde1f4088367f47e3669b6323e1e2914a68d
precioustosin <precioustosin@hotmail.com>
2018-03-29 10:18:00 +0100

    modified route to use hbs template

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   if (process.env.ENV === 'production') {
-    res.sendFile(path.join(__dirname, '../public_build/index.html'));
+    // res.sendFile(path.join(__dirname, '../public_build/index.html'));
   }
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+  res.render('post');
 });
 
 module.exports = router;
